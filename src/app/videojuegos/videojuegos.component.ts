38907e34c977c28d50334af1358843e148542882
Yan <yancertuche@gmail.com>
2021-02-22 13:59:13 -0500

    Using ng for

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -12,9 +12,13 @@ import {Component} from '@angular/core';
     <h3 *ngIf = "mostrar_retro != true"> 
     mejor juego :  {{mejor_juego}}
     </h3>
-    <h3 [style.background]={{color}} *ngIf = "mostrar_retro == true"> 
+    <h3 [style.background]="color" *ngIf = "mostrar_retro == true"> 
     mejor juego retro :  {{mejor_juego_retro}}
     </h3>
+    <h2>Listado</h2>
+    <ul>
+        <li *ngFor="let game of lista">{{game}}</li>
+    </ul>
     `  // The html code where will be show the compoent. The inverted quote marks let doing here
         // but we can import of other file
 })
@@ -23,4 +27,7 @@ export class VideojuegosComponent{
     public mejor_juego ='GTA 5';
     public mejor_juego_retro ='super mario 64';
     public mostrar_retro = true;
+    public color = "yellow"; 
+
+    public lista =['Hola','como', 'tas'];
 }

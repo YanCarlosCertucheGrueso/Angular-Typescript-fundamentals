097d73817b8b9c00bd421b334c11fe99ab885ece
Yan <yancertuche@gmail.com>
2021-02-22 12:35:23 -0500

    Using ngIf  condicional directive

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -7,10 +7,20 @@ import {Component} from '@angular/core';
     selector : 'videojuegos', // label where the component is load
     template : `
     <h3> 
-    componentes de videojuegos
+    componentes de {{nombre}}
+    </h3>
+    <h3 *ngIf = "mostrar_retro != true"> 
+    mejor juego :  {{mejor_juego}}
+    </h3>
+    <h3 [style.background]={{color}} *ngIf = "mostrar_retro == true"> 
+    mejor juego retro :  {{mejor_juego_retro}}
     </h3>
     `  // The html code where will be show the compoent. The inverted quote marks let doing here
+        // but we can import of other file
 })
 export class VideojuegosComponent{
-    public nombre = 'Video juegos 2021'
+    public nombre = 'Video juegos 2021';
+    public mejor_juego ='GTA 5';
+    public mejor_juego_retro ='super mario 64';
+    public mostrar_retro = true;
 }

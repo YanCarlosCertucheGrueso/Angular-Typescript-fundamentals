5450f2de217e07f0e19ee1e911932c1a25dda0f7
Yan <yancertuche@gmail.com>
2021-02-22 14:07:59 -0500

    individual views

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -5,21 +5,7 @@ import {Component} from '@angular/core';
 //note: it's not "herencia" because it don't create new components that " heredan" to the parent
 @Component({
     selector : 'videojuegos', // label where the component is load
-    template : `
-    <h3> 
-    componentes de {{nombre}}
-    </h3>
-    <h3 *ngIf = "mostrar_retro != true"> 
-    mejor juego :  {{mejor_juego}}
-    </h3>
-    <h3 [style.background]="color" *ngIf = "mostrar_retro == true"> 
-    mejor juego retro :  {{mejor_juego_retro}}
-    </h3>
-    <h2>Listado</h2>
-    <ul>
-        <li *ngFor="let game of lista">{{game}}</li>
-    </ul>
-    `  // The html code where will be show the compoent. The inverted quote marks let doing here
+    templateUrl : './videojuegos.component.html' // The html code where will be show the compoent. The inverted quote marks let doing here
         // but we can import of other file
 })
 export class VideojuegosComponent{
